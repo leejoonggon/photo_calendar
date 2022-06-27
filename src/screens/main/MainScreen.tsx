99afdd0e8139544c92270e5c80e24f1e0a727b67
Yoojin Lee <vermints74@gmail.com>
2022-06-27 17:44:52 +0900

    2022.06.27_yoojin

--- src/screens/main/MainScreen.tsx
+++ src/screens/main/MainScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, View, StatusBar, Image, TouchableOpacity, TextInput, Text, ViewBase, ShadowPropTypesIOS, Pressable } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 import { formatDiagnosticsWithColorAndContext } from 'typescript';
 import { DP, SP } from '../../util/size';
@@ -58,8 +59,98 @@ export class MainScreen extends React.Component<Props, State> {
                         />
                     </Pressable>
                 </View>
+            
+                {/* <ScrollView> */}
+                    <View style={styles.menubox}>
+                        <View style={styles.menutop}>
+                            <Image
+                                source={require('../../resources/search.png')}
+                                style={styles.searchicon}
+                            />
+                            <Image
+                                source={require('../../resources/set.png')}
+                                style={styles.seticon}
+                            />
+                        </View>
 
-                                      
+                        <View style={styles.nicknamebox}>
+                            <Text style={styles.nickname}>
+                                닉네임은이정도의길이면될듯
+                            </Text>
+                        </View>
+
+                        <View style={styles.myimage}/>
+                        
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                사진
+                            </Text>
+
+                            <Image
+                                source={require('../../resources/toggle_on.png')}
+                                style={styles.toggleonicon}
+                            />
+                        </View>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                일정
+                            </Text>
+
+                            <Image
+                                source={require('../../resources/toggle_off.png')}
+                                style={styles.toggleofficon}
+                            />
+                        </View>
+
+                        <View style={styles.line}/>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                전체 사진 보기
+                            </Text>
+                        </View>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                월 별 보기
+                            </Text>
+                        </View>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                일 별 보기
+                            </Text>
+                        </View>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                기본 캘린더
+                            </Text>
+                        </View>
+
+                        <View style={styles.line}/>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                가이드
+                            </Text>
+                        </View>
+
+                        <View style={styles.menutitlebox}>
+                            <Text style={styles.menutitle}>
+                                구매하기
+                            </Text>
+                        </View>
+
+                        <View style={styles.adline}/>
+
+                        <View style={styles.adbox_top}/>
+                    
+                        <View style={styles.adbox_bottom}/>
+
+                     </View>
+                {/* </ScrollView>          */}
             </View>
         )
     }
@@ -68,7 +159,8 @@ export class MainScreen extends React.Component<Props, State> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFFFFF', 
+        // backgroundColor: '#FFFFFF', 
+        backgroundColor: '#C4C4C4',
     },
 
     header_box: {
@@ -92,9 +184,133 @@ const styles = StyleSheet.create({
         height: DP(24),
         width: DP(24),
         marginLeft: DP(270),
+    },
 
-    }
+    menubox: {
+        backgroundColor: 'pink',
+        height: '100%',
+        position: 'absolute',
+        width: DP(256),
+        borderTopRightRadius: DP(16),
+        borderBottomRightRadius: DP(16),
+        // alignItems: 'center',
+    },
 
+    menutop: {
+        // backgroundColor: 'skyblue',
+        height: DP(48),
+        width: '100%',
+        marginTop: DP(24),
+        flexDirection: 'row',
+        // justifyContent: 'center',
+        alignItems: 'center',
+    },
 
+    searchicon: {
+        height: DP(24),
+        width: DP(24),
+        marginLeft: DP(176),
+        // backgroundColor: 'red',        
+    },
+
+    seticon: {
+        height: DP(24),
+        width: DP(24),
+        marginLeft: DP(16),
+        // backgroundColor: 'orange',
+    },
+
+    nicknamebox: {
+        // backgroundColor: 'orange',
+        height: DP(24),
+        width: '100%', 
+        marginBottom: DP(30),
+    },
+
+    nickname: {
+        // backgroundColor: 'white',
+        height: DP(24),
+        width: DP(154),
+        marginLeft: DP(86),
+        fontSize: SP(12),
+        color: '#383838',
+        textAlignVertical: 'center',
+    },
+
+    myimage: {
+        backgroundColor: 'green',
+        position: 'absolute',
+        height: DP(60),
+        width: DP(60),
+        marginLeft: DP(16),
+        marginTop: DP(36),
+        borderRadius: DP(70),
+    },
+
+    menutitlebox: {
+        // backgroundColor: 'brown',
+        height: DP(32),
+        width: '100%', 
+        flexDirection: 'row',
+        // justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    menutitle: {
+        // backgroundColor: 'red',
+        height: DP(24),
+        width: DP(138),
+        marginLeft: DP(16),
+        fontSize: SP(16),
+        color: '#383838',
+        textAlignVertical: 'center',
+    },
+
+    toggleonicon: {
+        height: DP(22),
+        width: DP(46),
+        marginLeft: DP(40),
+    },
+
+    toggleofficon: {
+        height: DP(22),
+        width: DP(46),
+        marginLeft: DP(40),
+    },
+
+    line: {
+        backgroundColor: '#EEEEEE',
+        height: DP(1),
+        width: DP(224),
+        marginLeft: DP(16),
+        marginTop: DP(8),
+        marginBottom: DP(8),
+    },
+
+    adline: {
+        backgroundColor: '#EEEEEE',
+        height: DP(1),
+        width: DP(224),
+        marginLeft: DP(16),
+        marginTop: DP(74),
+    },
+
+    adbox_top: {
+        backgroundColor: 'yellow',
+        height: DP(56),
+        width: DP(182),
+        marginLeft: DP(37),
+        marginTop: DP(16),
+        borderRadius: DP(4),
+    },
+
+    adbox_bottom: {
+        backgroundColor: 'orange',
+        height: DP(56),
+        width: DP(182),
+        marginLeft: DP(37),
+        marginTop: DP(8),
+        borderRadius: DP(4),
+    },
 
 })
